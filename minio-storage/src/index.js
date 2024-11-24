0150d3c9048c5d6847ab5347f646e7e10a3be12d
Salai Chit Oo Latt <salaichitoolatt@outlook.com>
2024-11-25 01:30:10 +0630

    Add object storage retrive service

--- minio-storage/src/index.js
+++ minio-storage/src/index.js
@@ -3,6 +3,7 @@ import * as Minio from "minio";
 
 let ACCESS_KEY = "duWkd7MGEI8KtWpt7kPW";
 let SECRET_KEY = "16HlgYxFNQOfgwJWea5REHGXqbWcEPykIrJKDl53";
+let bucketName = process.env.BUCKET_NAME || "bmdk1";
 const PORT = process.env.PORT || 3000;
 
 const minioClient = new Minio.Client({
@@ -19,22 +20,46 @@ app.get("/video", async (req, res) => {
   try {
     const videoPath = req.query.path;
 
-    const bucketName = "bmdk1";
-    //
-    const data = await minioClient.bucketExists(bucketName);
-    if (!data) {
-      console.log("Bucket not exists.");
-      return res.send("Bucket Not Found");
+    const bucketExists = await minioClient.bucketExists(bucketName);
+    if (!bucketExists) {
+      console.log("Bucket does not exist.");
+      return res.status(404).send("Bucket Not Found");
     }
-    
-    const dataStream = await minioClient.getObject(bucketName, videoPath)
 
+    try {
+      const stat = await minioClient.statObject(bucketName, videoPath);
+      if (!stat) {
+        res.status(404).send("Object Not Found");
+      }
+
+      let size = 0;
+      const dataStream = await minioClient.getObject(bucketName, videoPath);
+      dataStream.on("data", function (chunk) {
+        size += chunk.length;
+      });
+
+      res.setHeader("Content-Type", "video/mp4");
+      res.setHeader("Accept-Ranges", "bytes");
+
+      dataStream.on("end", function () {
+        console.log("End. Total Object size = " + size);
+      });
+      
+      dataStream.on("error", function (err) {
+        console.log(err);
+      });
+
+      dataStream.pipe(res);
+    } catch (error) {
+      console.log("Error fetching the video:", error);
+      return res.status(404).send("Video not found");
+    }
   } catch (error) {
-    console.log(error);
+    console.log("Error checking bucket:", error);
+    return res.status(500).send("Internal Server Error");
   }
-
 });
 
 app.listen(PORT, () =>
-  console.log(`Minio Blob Storage  microservice online 🚀🚀🚀`)
+  console.log(`Minio Blob Storage microservice online 🚀🚀🚀`)
 );
