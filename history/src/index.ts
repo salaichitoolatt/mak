eebe1c7b7361b279c2f8723fc3410cbad591a960
Salai Chit Oo Latt <salaichitoolatt@outlook.com>
2024-11-29 00:16:36 +0630

    Add amqp to the history microservice

--- history/src/index.ts
+++ history/src/index.ts
@@ -1,10 +1,18 @@
-import express, {Request, Response } from "express";
+import express, { Request, Response } from "express";
+import amqp from "amqplib";
 
-const app = express();
 const PORT = process.env.PORT;
+const RABBIT = process.env.RABBIT as string;
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello");
+async function main() {
+  const app = express();
+  const messagingConnection = await amqp.connect(RABBIT);
+  const messageChannel = await messagingConnection.createChannel();
+
+  app.listen(PORT);
+}
+
+main().catch((err) => {
+  console.error("Microservice failed to start.");
+  console.error((err && err.stack) || err);
 });
-
-app.listen(PORT);
