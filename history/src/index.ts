65d477a62688647ef3355656a6eebb30765ac7ec
Salai Chit Oo Latt <salaichitoolatt@outlook.com>
2024-12-01 23:39:36 +0630

    Add consuming viewed message from queue

--- history/src/index.ts
+++ history/src/index.ts
@@ -1,13 +1,34 @@
 import express, { Request, Response } from "express";
-import amqp from "amqplib";
+import amqp, { ConsumeMessage, Message } from "amqplib";
+import mongodb from "mongodb";
 
 const PORT = process.env.PORT;
 const RABBIT = process.env.RABBIT as string;
+const DBHOST = process.env.DBHOST as string;
+const DBNAME = process.env.DBNAME;
 
 async function main() {
+  const mongoClient = await mongodb.MongoClient.connect(DBHOST);
+  const db = mongoClient.db(DBNAME);
+  const historyCollection = db.collection("history");
+
   const app = express();
   const messagingConnection = await amqp.connect(RABBIT);
   const messageChannel = await messagingConnection.createChannel();
+
+  await messageChannel.assertQueue("viewed", {});
+
+  await messageChannel.consume("viewed", async (msg: ConsumeMessage | null) => {
+    if (msg) {
+      const parsedMsg = JSON.parse(msg?.content.toString() as string);
+
+      await historyCollection.insertOne({
+        videoPath: parsedMsg.videoPath,
+      });
+
+      messageChannel.ack(msg);
+    }
+  });
 
   app.listen(PORT);
 }
