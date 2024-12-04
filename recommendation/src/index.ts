6d22e349fe36ec8b22cfa4e5a47f9820d943fe97
Salai Chit Oo Latt <salaichitoolatt@outlook.com>
2024-12-04 09:04:32 +0630

    Add PORT var to the recommendation microservice

--- recommendation/src/index.ts
+++ recommendation/src/index.ts
@@ -2,8 +2,10 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
+const PORT = process.env.PORT;
+
 app.get("/status", (req: Request, res: Response) => {
   res.send("Service online.");
 });
 
-app.liste(3000, () => console.log("Microservice online."));
+app.listen(PORT, () => console.log("Microservice online."));
